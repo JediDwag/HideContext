fa8ffe4e240aa0b5afc7079908837e443cb8b47d
Nathan Hickey <hickey.nathan@gmail.com>
2020-11-08 13:10:53 -0600

    New version, initial commit

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({hide: true}, function() {
-      console.log("Hide image is on");
+      console.log("Hide context is on");
     });
   });
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'www.quora.com'},
+        pageUrl: {hostEquals: 'www.youtube.com'},
       })
       ],
           actions: [new chrome.declarativeContent.ShowPageAction()]
